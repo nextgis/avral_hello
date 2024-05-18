f1e0709a357c0f89d2be9a1f6005f2347f96a6a2
Maxim Dubinin <maxim.dubinin@nextgis.com>
2024-05-18 10:08:07 +0300

    helloworld -> hello

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-import os
-
 from setuptools import setup
-
-# here = os.path.abspath(os.path.dirname(__file__))
-# with open(os.path.join(here, 'README.txt')) as f:
-#     README = f.read()
-# with open(os.path.join(here, 'CHANGES.txt')) as f:
-#     CHANGES = f.read()
 
 requires = [
     'avral',
@@ -16,22 +8,20 @@ setup(
     name='avral_helloworld',
     version='0.0.1',
     description='Extension for NextGIS Distributed Geo Task',
-    # long_description=README + '\n\n' + CHANGES,
     classifiers=[
         "Programming Language :: Python",
     ],
     author='nextgis',
     author_email='info@nextgis.com',
-    url='http://nextgis.com',
+    url='https://nextgis.com',
     keywords='geo processing server',
-    packages=['avral_helloworld'],
+    packages=['avral_hello'],
     include_package_data=True,
     zip_safe=False,
-    # test_suite='avral_web',
     install_requires=requires,
     entry_points={
         'avral_operations': [
-            'hello = avral_helloworld.operations:HelloWorld',
+            'hello = avral_hello.operations:HelloWorld',
         ],
     }
 )
