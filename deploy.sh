2a65057c13553651c5989a51e9dbe62bc627b815
Maxim Dubinin <maxim.dubinin@nextgis.com>
2024-08-14 14:26:43 +0300

    oneline deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,1 @@
-docker build --no-cache -t avral_hello:latest  .
-docker tag avral_hello:latest harbor.nextgis.net/toolbox-workers/hello:prod
-docker image push harbor.nextgis.net/toolbox-workers/hello:prod
+docker build --no-cache -t harbor.nextgis.net/toolbox-workers/hello:prod --push .
